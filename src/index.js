e304e428b1ab31c37635918b68f9fe1409b1c7d6
Pavel Cherevach <pf4pad@gmail.com>
2021-12-29 13:02:56 +0200

    feat: create task reverse-numbers

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+  a = String(Math.abs(n))
+
+  let revers = Number(a.split('').reverse().join(''))
+  return revers
+
 }
